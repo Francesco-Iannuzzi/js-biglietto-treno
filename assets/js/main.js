9029136fd3fbc0e49e80a8063047913c6f6015a1
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-03-08 14:51:47 +0100

    add tools + add prompt and calc price

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,8 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 - il prezzo del biglietto è definito in base ai km (0.21 € al km)
 - va applicato uno sconto del 20% per i minorenni
 - va applicato uno sconto del 40% per gli over 65.
-L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+Questo richiederà un minimo di ricerca.
 */
 
 /* Strumenti:
@@ -13,11 +14,28 @@ L'output del prezzo finale va messo fuori in forma umana (con massimo due decima
     - console-log
     - math
     - if / else
+    - getElementById
 */
 
 //chiedere all'utente i km da percorrere e registrarli
-//chuedere all'utente l'età e registrarla
+const kilometres = Number(prompt('Quanti chilometri dovrai percorrere?'));
+console.log(kilometres);
+
+//chiedere all'utente l'età e registrarla
+const userAge = Number(prompt('Quanti anni hai?'));
+console.log(userAge);
+
+//definire la variabile del prezzo al chilomentro (euro 0,21)
+const pricePerKilometre = 0.21
+console.log(pricePerKilometre);
+
+//calcolare il prezzo del biglietto moltiplicando il numero di chilometri per il prezzo al chilometro
+const price = kilometres * pricePerKilometre;
+console.log(price);
+
 //verificare se l'utente ha un'età inferiore ai 18 anni ed applicare lo sconto del 20%
 //verificare se l'utente ha un'età superiore ai 65 anni ed applicare lo sconto del 40%
 //altrimenti non applicare nessuno sconto
+//aggiungere un h1 con id per mostrare il prezzo in schermata
+//selezionare e salvare in una variabile l'elemento della dom con getElementById
 //comunicare all'utente il costo del biglietto con massimo due decimali per indicare i centesimi sul prezzo
