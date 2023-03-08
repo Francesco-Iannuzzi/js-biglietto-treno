fe82793c03e44ca0b4650e1439d405463abc935d
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-03-08 15:50:30 +0100

    add calc discount + resukt on desktop user

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ Questo richiederà un minimo di ricerca.
 */
 
 /* Strumenti:
-    - number prompt
+    - number() prompt
     - const / let
     - console-log
     - math
@@ -26,16 +26,27 @@ const userAge = Number(prompt('Quanti anni hai?'));
 console.log(userAge);
 
 //definire la variabile del prezzo al chilomentro (euro 0,21)
-const pricePerKilometre = 0.21
+const pricePerKilometre = 0.21;
 console.log(pricePerKilometre);
 
 //calcolare il prezzo del biglietto moltiplicando il numero di chilometri per il prezzo al chilometro
-const price = kilometres * pricePerKilometre;
+let price = kilometres * pricePerKilometre;
 console.log(price);
 
-//verificare se l'utente ha un'età inferiore ai 18 anni ed applicare lo sconto del 20%
-//verificare se l'utente ha un'età superiore ai 65 anni ed applicare lo sconto del 40%
-//altrimenti non applicare nessuno sconto
+//verificare se l'utente ha un'età inferiore ai 18 anni ed applicare lo sconto del 20% o se l'utente ha un'età superiore ai 65 anni ed applicare lo sconto del 40% altrimenti non applicare nessuno sconto (dare un prezzo con massimo due numeri decimali)
+if (userAge < 18) {
+    price = price - (price * 0.20).toFixed();
+    
+} else if (userAge > 65) {
+    price = price - (price * 0.40).toFixed();
+} else {
+    price = price
+}
+
 //aggiungere un h1 con id per mostrare il prezzo in schermata
+
 //selezionare e salvare in una variabile l'elemento della dom con getElementById
-//comunicare all'utente il costo del biglietto con massimo due decimali per indicare i centesimi sul prezzo
+const nameElement = document.getElementById('finalPrice')
+
+//comunicare all'utente il costo del biglietto
+nameElement.innerHTML=`Il prezzo per il tuo biglietto è di ${price} euro`;
